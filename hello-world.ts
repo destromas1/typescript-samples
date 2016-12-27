706d2e0615bffd166ad3e2cded51c89ff7742483
destromas1 <shahjada.talukdar@gmail.com>
2016-12-28 01:38:17 +0600

    hello-world.ts

---
+++ hello-world.ts
@@ -0,0 +1,10 @@
+// Any Valid JS is valid TS
+
+function greeter(person) {
+    return "Hello, " + person;
+}
+
+const user = "Shahjada Talukdar";
+
+const result = greeter(user);
+console.log(result);
